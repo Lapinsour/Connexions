6649f235a0bd93382df9596feef376e631a21e23
Pierre Garrigues <141630659+Lapinsour@users.noreply.github.com>
2026-04-16 14:59:33 +0200

    Update categories.py

--- categories.py
+++ categories.py
@@ -2,7 +2,7 @@ CATEGORIES = [
     {"name": "Trucs à carapace", "words": ["Tortue", "Calebasse", "Scarabée", "Tank"]},
     {"name": "Trucs jaune et noir", "words": ["Banane mûre", "Stade Rochelais", "Dalton", "Abeille"]},
     {"name": "Fruits de comptine", "words": ["Pomme", "Pêche", "Poire", "Abricot"]},
-    {"name": "", "words": ["Morange", "Pivert", "Aigris", "Lazure"]}
+    {"name": "Finissant en une couleur", "words": ["Morange", "Pivert", "Aigris", "Lazure"]}
     
     
 ]
