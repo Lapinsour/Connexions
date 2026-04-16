ed0e3139bcb89e0d570830d91d59d2280b6e275c
Pierre Garrigues <141630659+Lapinsour@users.noreply.github.com>
2026-04-16 14:31:14 +0200

    Create categories.py

---
+++ categories.py
@@ -0,0 +1,9 @@
+CATEGORIES = [
+    {"name": "animaux domestiques", "words": ["chien", "chat", "lapin", "hamster"], "decoys": ["souris", "python"]},
+    {"name": "animaux sauvages", "words": ["lion", "tigre", "loup", "ours"], "decoys": ["python", "jaguar"]},
+    {"name": "fruits", "words": ["pomme", "banane", "poire", "orange"], "decoys": ["cerise", "fraise"]},
+    {"name": "couleurs", "words": ["rouge", "bleu", "vert", "orange"], "decoys": ["rose", "violet"]},
+    {"name": "informatique", "words": ["souris", "clavier", "écran", "serveur"], "decoys": ["cloud", "java"]},
+    {"name": "langages de programmation", "words": ["python", "java", "ruby", "go"], "decoys": ["c", "rust"]},
+    
+]
