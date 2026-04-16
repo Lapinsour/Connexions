9ad5294e7820452ff21217ea698b8f45e5ae5e03
Pierre Garrigues <141630659+Lapinsour@users.noreply.github.com>
2026-04-16 15:02:56 +0200

    Update validator.py

--- validator.py
+++ validator.py
@@ -1,26 +1,15 @@
 def check_group(selected_words, puzzle):
-    """
-    selected_words : liste de 4 mots sélectionnés
-    puzzle : dictionnaire retourné par generator.py
-    Retourne :
-        {correct: bool, category: str ou None}
-    """
-    selected_set = set(selected_words)
+
+    selected = set(selected_words)
 
     for group in puzzle["groups"]:
-        group_set = set(group["words"])
-        if selected_set == group_set:
-            return {"correct": True, "category": group["category"]}
+        if selected == set(group["words"]):
+            return {
+                "correct": True,
+                "category": group["category"]
+            }
 
-    return {"correct": False, "category": None}
-
-
-# Test rapide
-if __name__ == "__main__":
-    from generator import generate_puzzle
-    puzzle = generate_puzzle()
-    print("Grille :", puzzle["words"])
-    # Exemple de test
-    test = puzzle["groups"][0]["words"]
-    result = check_group(test, puzzle)
-    print("Test validation :", result)
+    return {
+        "correct": False,
+        "category": None
+    }
