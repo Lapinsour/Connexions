1ebaa8ecc6800b7bcc0399d8dbb757e3a68ef2cd
Pierre Garrigues <141630659+Lapinsour@users.noreply.github.com>
2026-04-16 14:58:17 +0200

    Update categories.py

--- categories.py
+++ categories.py
@@ -1,9 +1,8 @@
 CATEGORIES = [
-    {"name": "animaux domestiques", "words": ["chien", "chat", "lapin", "hamster"], "decoys": ["souris", "python"]},
-    {"name": "animaux sauvages", "words": ["lion", "tigre", "loup", "ours"], "decoys": ["python", "jaguar"]},
-    {"name": "fruits", "words": ["pomme", "banane", "poire", "orange"], "decoys": ["cerise", "fraise"]},
-    {"name": "couleurs", "words": ["rouge", "bleu", "vert", "orange"], "decoys": ["rose", "violet"]},
-    {"name": "informatique", "words": ["souris", "clavier", "écran", "serveur"], "decoys": ["cloud", "java"]},
-    {"name": "langages de programmation", "words": ["python", "java", "ruby", "go"], "decoys": ["c", "rust"]}
+    {"name": "Trucs à carapace", "words": ["Tortue", "Calebasse", "Scarabée", "Tank"]},
+    {"name": "Trucs jaune et noir", "words": ["Banane mûre", "Stade Rochelais", "Dalton", "Abeille"]},
+    {"name": "Fruits de comptine", "words": ["Pomme", "Pêche", "Poire", "Abricot"]},
+    {"name": "", "words": ["Morange", "Pivert", "Aigris", "Lazure"]}
+    
     
 ]
