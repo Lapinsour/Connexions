ceccdca4126abbce9c134ae63d8f31ab7f99169c
Pierre Garrigues <141630659+Lapinsour@users.noreply.github.com>
2026-04-16 14:31:24 +0200

    Update categories.py

--- categories.py
+++ categories.py
@@ -4,6 +4,6 @@ CATEGORIES = [
     {"name": "fruits", "words": ["pomme", "banane", "poire", "orange"], "decoys": ["cerise", "fraise"]},
     {"name": "couleurs", "words": ["rouge", "bleu", "vert", "orange"], "decoys": ["rose", "violet"]},
     {"name": "informatique", "words": ["souris", "clavier", "écran", "serveur"], "decoys": ["cloud", "java"]},
-    {"name": "langages de programmation", "words": ["python", "java", "ruby", "go"], "decoys": ["c", "rust"]},
+    {"name": "langages de programmation", "words": ["python", "java", "ruby", "go"], "decoys": ["c", "rust"]}
     
 ]
